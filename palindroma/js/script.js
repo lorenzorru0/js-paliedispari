03a6c33835ddc5807210bd5dfaed6daac798fc5b
lorenzorru0 <lorenzoorru999@gmail.com>
2021-08-31 16:23:45 +0200

    Add al the style

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -13,15 +13,24 @@ var buttonVerify = document.getElementById("verify");
 buttonVerify.addEventListener("click", 
     function() {
         var str = document.getElementById("string").value;
-        str = str[0].toLowerCase() + str.slice(1);
+        console.log(str);
         var result = document.getElementById("result");
-        if (str == "") {
-            result = "Errore! Inserisci la parola";
+        if (str.length < 3) {
+            if (str == "") {
+                result.innerHTML = "Errore! Inserisci una parola";
+                result.classList.add("view");
+            } else {
+                result.innerHTML = "Errore! Inserisci una parola più lunga";
+                result.classList.add("view");
+            }
         } else {
+            str = str[0].toLowerCase() + str.slice(1);
             if (isPalindrome(str)) {
                 result.innerHTML = "La parola è palindroma";
+                result.classList.add("view");
             } else {
                 result.innerHTML = "La parola non è palindroma";
+                result.classList.add("view");
             }
         }
     }
